927dae0032aec2bae763a22355873ef6941e3b18
Rajendra Jat <jatrj18@gmail.com>
2025-08-25 06:02:57 +0530

    project setup

--- backend/backend-app/src/main.ts
+++ backend/backend-app/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.ORIGIN || '*',
+    origin: process.env.ORIGIN || 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
