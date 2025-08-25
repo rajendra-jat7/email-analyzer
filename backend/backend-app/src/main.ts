88e1cacce6977bc6942ebdb1973e48615ce23cb4
Rajendra Jat <jatrj18@gmail.com>
2025-08-25 05:53:27 +0530

    project setup

--- backend/backend-app/src/main.ts
+++ backend/backend-app/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
 
-  const origins = (process.env.ORIGIN || '*').split(',').map((o) => o.trim());
-  app.enableCors({ origin: origins, credentials: true });
+  app.enableCors({
+    origin: process.env.ORIGIN || '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
